b869bea592f48b7361ae9eff4d58156fb377501c
Warren Joseph Ramos <wjnramos@gmail.com>
2021-11-08 10:56:40 -0700

    commented out sign in and sign out to work on dashboard design

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
-import React from 'react';
-import './App.css';
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
-import Amplify from 'aws-amplify';
-import aws_exports from './aws-exports';
-import DashBoard from './components/DashBoard'
+import React from "react";
+import "./App.css";
+//import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+//import Amplify from 'aws-amplify';
+//import aws_exports from './aws-exports';
+import DashBoard from "./components/DashBoard";
 
-Amplify.configure(aws_exports);
+//Amplify.configure(aws_exports);
 
-function App () {
-    return (
-      <div className="App">
-        <AmplifySignOut />
-          <DashBoard>
-          </DashBoard>
-      </div>
-    );
+function App() {
+  // <div className="App"><AmplifySignOut /><DashBoard /></div>
+
+  return (
+    <div className="App">
+      <DashBoard>
+        <h1>hello</h1>
+      </DashBoard>
+    </div>
+  );
 }
 
-export default withAuthenticator(App);
+//export default withAuthenticator(App);
+export default App;
