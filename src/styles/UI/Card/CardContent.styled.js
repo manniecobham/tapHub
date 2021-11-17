3b55583cb6be7ce86062583ea4ca89ae032c269a
WarrenJRamos <wjnramos@gmail.com>
2021-11-16 20:18:16 -0700

    Removed unecessary files from the repository

--- src/styles/UI/Card/CardContent.styled.js
+++ src/styles/UI/Card/CardContent.styled.js
@@ -3,7 +3,8 @@ import styled from "styled-components";
 const CardContent = styled.div`
   .lead {
     font-size: 40px;
-    font-weight: 500;
+    font-weight: bold;
+    color: #172c41;
   }
 `;
 
